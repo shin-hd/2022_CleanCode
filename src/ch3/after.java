2d708cca87749c29cc0f24040afcb05631b32bec
shinhd <shinhd1998@gmail.com>
2022-08-17 13:08:16 +0900

    after

---
+++ src/ch3/after.java
@@ -0,0 +1,35 @@
+package ch3;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class after {
+    List<String> words = new ArrayList<>();
+
+    public void print() {
+        words.forEach(word -> System.out.println(word));
+    }
+
+    // 단어가 기존 리스트에 있는지 확인
+    // after.hasWord(word);
+    public boolean hasWord(String word) {
+        if (words.stream().filter(w -> w.equalsIgnoreCase(word)).count() > 0)
+            return true;
+        else return false;
+    }
+
+    // 단어 추가만
+    public void addWord(String word) {
+        if (!hasWord(word))
+            words.add(word.toLowerCase());
+    }
+
+    // 가변인자 사용
+    public void addWords(String... words) {
+        for (String word : words) {
+            addWord(word);
+        }
+    }
+
+}
