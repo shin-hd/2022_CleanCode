88b078af1f9851a0b82e4d662963a3fba26dc6d1
shinhd <shinhd1998@gmail.com>
2022-08-17 13:08:06 +0900

    before

---
+++ src/ch3/before.java
@@ -0,0 +1,44 @@
+package ch3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class before {
+    List<String> words = new ArrayList<>();
+
+    public void print() {
+        words.forEach(word -> System.out.println(word));
+    }
+
+    // 한번에 여러가지 수행
+    // 추상화 수준이 일정하지 않음
+    // 명령과 조회가 동시에
+    // 중복
+    public boolean addWordAndPrint(String word) {
+        if (words.stream().filter(w -> w.equalsIgnoreCase(word)).count() == 0) {
+            words.add(word.toLowerCase());
+            print();
+            return true;
+        }
+        else {
+            print();
+            return false;
+        }
+    }
+
+    // 많고 동일한 타입의 옵셔널 매개변수
+    // 중복
+    public void words(String w1, String w2, String w3) {
+        if (!"".equals(w1))
+            if (words.stream().filter(w -> w.equalsIgnoreCase(w1)).count() == 0)
+                words.add(w1.toLowerCase());
+
+        if (!"".equals(w2))
+            if (words.stream().filter(w -> w.equalsIgnoreCase(w2)).count() == 0)
+                words.add(w2.toLowerCase());
+
+        if (!"".equals(w3))
+            if (words.stream().filter(w -> w.equalsIgnoreCase(w3)).count() == 0)
+                words.add(w3.toLowerCase());
+    }
+}
